47eb69d8cd85ea0a23a60049cd0ca709a7eeec54
Bittor Poza <bipoza@gmail.com>
2021-05-14 09:22:00 +0200

    Volto crash if you don't initialize content from Plone (#22)
    * check if initial value is array before reduce
    
    * initialize title of defaultMenuConfiguration

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,14 @@
 export function getItemsByPath(items, pathname) {
   let rootPathConfig = null;
-  const itemsByPath = items?.reduce((acc, val) => {
-    if (val.rootPath === '/') {
-      rootPathConfig = val;
-      return acc;
-    }
-    return { ...acc, [val.rootPath]: val };
-  }, {});
+  const itemsByPath = Array.isArray(items)
+    ? items?.reduce((acc, val) => {
+        if (val.rootPath === '/') {
+          rootPathConfig = val;
+          return acc;
+        }
+        return { ...acc, [val.rootPath]: val };
+      }, {})
+    : [];
   const matchingPaths = Object.keys(itemsByPath)
     .filter((path) => pathname.startsWith(path))
     .sort((a, b) => {
