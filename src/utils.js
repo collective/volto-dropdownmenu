3355a853c51b7bb5821b020ca867ccc6ee997ef3
Sabrina Bongiovanni <sabrina.bongiovanni@redturtle.it>
2024-03-12 16:52:25 +0100

    initial commit

--- src/utils.js
+++ src/utils.js
@@ -9,13 +9,29 @@ export function getItemsByPath(items, pathname) {
         return { ...acc, [val.rootPath]: val };
       }, {})
     : [];
+
+  console.log(itemsByPath, 'itemsByPath');
+
   const matchingPaths = Object.keys(itemsByPath)
-    .filter((path) => `${pathname.trim('/')}/`.includes(`${path}/`))
+    .filter((path) => {
+      console.log(path, 'path');
+      console.log(pathname, 'pathname - parametro');
+      console.log(`${pathname.trim('/')}/`.startsWith(`${path}/`));
+      return `${pathname.trim('/')}/`.includes(`${path}/`);
+    })
     .sort((a, b) => {
       if (a.length > b.length) return -1;
       else if (a.length < b.length) return 1;
       else return 0;
     });
+
+  // const matchingPaths = Object.keys(itemsByPath)
+  //   .filter((path) => `${pathname.trim('/')}/`.includes(`${path}/`))
+  //   .sort((a, b) => {
+  //     if (a.length > b.length) return -1;
+  //     else if (a.length < b.length) return 1;
+  //     else return 0;
+  //   });
 
   if (matchingPaths.length > 0) return itemsByPath[matchingPaths[0]].items;
   else if (rootPathConfig) return rootPathConfig.items;
