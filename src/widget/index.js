aeaa1ada6e5d8f024a2fc739161d496b08d76ae5
Giulia Ghisini <giulia.ghisini@redturtle.it>
2024-04-04 11:25:04 +0200

    fix: import name

--- src/widget/index.js
+++ src/widget/index.js
@@ -2,18 +2,18 @@ import loadable from '@loadable/component';
 
 export const MenuConfigurationForm = loadable(() =>
   import(
-    /* webpackChunkName: "volto-dropdownmenu-manage" */ 'volto-dropdownmenu/widget/MenuConfigurationForm'
+    /* webpackChunkName: "volto-dropdownmenu-manage" */ './widget/MenuConfigurationForm'
   ),
 );
 
 export const MenuConfigurationWidget = loadable(() =>
   import(
-    /* webpackChunkName: "volto-dropdownmenu-manage" */ 'volto-dropdownmenu/widget/MenuConfigurationWidget'
+    /* webpackChunkName: "volto-dropdownmenu-manage" */ './widget/MenuConfigurationWidget'
   ),
 );
 
 export const RadioWidget = loadable(() =>
   import(
-    /* webpackChunkName: "volto-dropdownmenu-manage" */ 'volto-dropdownmenu/widget/RadioWidget'
+    /* webpackChunkName: "volto-dropdownmenu-manage" */ './widget/RadioWidget'
   ),
 );
